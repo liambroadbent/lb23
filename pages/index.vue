008d2617e9afe04d2bc5cef5d180c59ed01abd86
Liam <lbroadbent21+github@gmail.com>
2023-01-07 09:04:27 +0000

    added little hover effect

--- pages/index.vue
+++ pages/index.vue
@@ -4,8 +4,8 @@
 
 <template>
     <div class="page">
-        <h1>Liam Broadbent?</h1>
-        <a href="https://www.linkedin.com/in/liambroadbent/">Get in touch</a>
+        <h1>Liam Broadbent</h1>
+        <a href="https://www.linkedin.com/in/liambroadbent/" id="style-2" data-replace="Get in touch"><span>Get in touch</span></a>
         <ClientOnly>
             <TheExperience />
         </ClientOnly>
@@ -35,6 +35,54 @@ h1 {
     font-weight: 900;
     font-size: clamp(1.5rem, 3.5vw, 7rem);
     text-transform: uppercase;
+}
+
+a {
+  overflow: hidden;
+  position: relative;
+  display: inline-block;
+}
+
+a::before,
+a::after {
+ content: '';
+  position: absolute;
+  width: 100%;
+  left: 0;
+}
+a::before {
+  background-color: #e1f0c2;
+  height: 2px;
+  bottom: 0;
+  transform-origin: 100% 50%;
+  transform: scaleX(0);
+  transition: transform .5s cubic-bezier(0.76, 0, 0.24, 1);
+}
+a::after {
+  content: attr(data-replace);
+  height: 100%;
+  top: 0;
+  transform-origin: 100% 50%;
+  transform: translate3d(200%, 0, 0);
+  transition: transform .5s cubic-bezier(0.76, 0, 0.24, 1);
+  color: #e1f0c2;
+}
+
+a:hover::before {
+  transform-origin: 0% 50%;
+  transform: scaleX(1);
+}
+a:hover::after {
+  transform: translate3d(0, 0, 0);
+}
+
+a span {
+  display: inline-block;
+  transition: transform .5s cubic-bezier(0.76, 0, 0.24, 1);
+}
+
+a:hover span {
+  transform: translate3d(-200%, 0, 0);
 }
 
 a {
